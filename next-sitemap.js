e263df15173b640876d30857c7f4f78b6a166089
alcaroff <acaroff10@gmail.com>
2021-12-12 18:12:46 +0100

    sitemap

---
+++ next-sitemap.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteUrl: process.env.SITE_URL || "https://tweetpreview.alca.app",
+  generateRobotsTxt: true, // (optional)
+  // exclude: ["/404", "/500", "/admin"],
+  // ...other options
+};
